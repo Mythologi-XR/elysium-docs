3db9b299a5dc31504fdd1df6993c29bf44dab7cc
Paul Hine <paul@hine.works>
2022-07-01 16:32:44 +0200

    image urls 2

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 type FeatureItem = {
   title: string;
@@ -11,33 +12,33 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Elysium: the AR authoring engine',
-    imgSrc: '@site/static/img/app-slides/elysium-1.jpg',
+    imgSrc: '/img/app-slides/elysium-1.jpg',
   },
   {
     title: 'Create AR experiences',
-    imgSrc: '@site/static/img/app-slides/elysium-2.jpg',
+    imgSrc: '/img/app-slides/elysium-2.jpg',
   },
   {
     title: 'Browse & collect 3D assets',
-    imgSrc: '@site/static/img/app-slides/elysium-3.jpg',
+    imgSrc: '/img/app-slides/elysium-3.jpg',
   },
   {
     title: 'Tether your work to the real world',
-    imgSrc: '@site/static/img/app-slides/elysium-4.jpg',
+    imgSrc: '/img/app-slides/elysium-4.jpg',
   },
   {
     title: 'Manage images, 3D art and more',
-    imgSrc: '@site/static/img/app-slides/elysium-5.jpg',
+    imgSrc: '/img/app-slides/elysium-5.jpg',
   },
   {
     title: 'Add real-world interaction',
-    imgSrc: '@site/static/img/app-slides/elysium-6.jpg',
+    imgSrc: '/img/app-slides/elysium-6.jpg',
   },
 ];
 
 function Feature({title, imgSrc, description}: FeatureItem) {
   return (
-    <img className={styles.featureImage} src={imgSrc} title={title} />
+    <img className={styles.featureImage} src={useBaseUrl(imgSrc)} title={title} />
   );
 }
 
