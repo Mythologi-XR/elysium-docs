fa7cf46a71fc6eacfb0b4a55ba45492ba600df46
Paul Hine <paul@hine.works>
2022-07-20 14:10:16 +0200

    pricing page style

--- src/components/Pricing/PricingCard.tsx
+++ src/components/Pricing/PricingCard.tsx
@@ -39,9 +39,9 @@ export const PricingCard = ({
 } : PricingCardProps ) => (
   <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
     <h3 className="mb-4 text-2xl font-semibold"><Markdown>{title}</Markdown></h3>
-    <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
       <Markdown>{description}</Markdown>
-    </p>
+    </div>
     <div className="flex justify-center items-baseline my-8">
         <span className="mr-2 text-5xl font-extrabold">{price}</span>
         <span className="text-gray-500 dark:text-gray-400">{pricePeriod}</span>
