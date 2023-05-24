f0565ede78b3ddc2862a04807d0967f342a326e3
Monica Rekas <101420263+johnny-mnemonica@users.noreply.github.com>
2023-05-24 17:16:52 -0400

    mob styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ import { useMediaQuery } from 'react-responsive'
 
 export default function App() {
 
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  // const isDesktop = useMediaQuery({ query: '(max-width: 768px)' })
 
   return (
     <div className={styles.background}>
 
       {/* socials */}
-      <div className='absolute right-0 bottom-0 z-50 mb-3 mr-3 flex flex-row backdrop-blur-md bg-[rgb(234,255,195)]/20 rounded-full p-5'>
+      <div className='absolute md:right-0 bottom-0 z-50 mb-3 mr-3 flex flex-row backdrop-blur-md bg-[rgb(234,255,195)]/20 rounded-full p-5 max-w-sm'>
         <a href="https://www.instagram.com/elysium.xr/" target='_blank'><InstaLogo className='mr-4'/></a>
         <a href="https://twitter.com/elysiumXR" target='_blank'><TwitterLogo className='mr-4'/></a>
         <a href="https://www.linkedin.com/company/elysiumxr/" target='_blank'><LinkedinLogo className='mr-3'/></a>
@@ -25,11 +25,8 @@ export default function App() {
       
       {/* elysium logo */}
       <div className='absolute left-0 top-0 z-50 flex flex-row mt-[.6rem] ml-[.2rem] '>
-        {
-         !isMobile ?
-        <img src={useBaseUrl('img/elysium-logo-light.png')} width="300"/>
-        : <img src={useBaseUrl('img/elysium-logo-light-mob.png')} width="97"/>
-        }
+        <img src={useBaseUrl('img/elysium-logo-light.png')} width="300" className='hidden md:inline-block'/>
+        <img src={useBaseUrl('img/elysium-logo-light-mob.png')} width="97" className='md:hidden inline-block'/>
         {/* <img src={useBaseUrl('img/Elysium-Full_dark.png')} width="300"/> */}
       </div>
 
@@ -46,7 +43,7 @@ export default function App() {
             Join the waitlist to receive updates about our next launch.
           </span>
           <div className='self-center mt-9'>
-            <PopupButton id="BzV7hDhi" className={`self-center rounded-2xl bg-lime-100 hover:bg-cyan-200 hover:transition-all hover:ease-in-out border-solid border-white border-[1px] flex hover:duration-500 duration-500 cursor-pointer ${styles.cta}`}>
+            <PopupButton id="BzV7hDhi" className={`self-center rounded-2xl bg-lime-100 md:hover:bg-cyan-200 hover:transition-all hover:ease-in-out border-solid border-white border-[1px] flex hover:duration-500 duration-500 cursor-pointer ${styles.cta}`}>
                 Join the waitlist
             </PopupButton>
           </div>
